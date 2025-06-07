3105c8611a8cf31d22c7f519bc0159ffa3bc07a0
VangeFranco <vangefranco@gmail.com>
2025-06-07 15:26:23 -0300

    Add files via upload

--- bmi.c
+++ bmi.c
@@ -13,28 +13,14 @@ int main()
 	
 	imc=peso/(altura*altura);
 	
-	printf("\nSu indice de masa corporal es %f \n\n",imc);
+	printf("Su indice de masa corporal es %f \n\n",imc);
 	
 	printf("Indice          Condición\n");
 	printf("---------------------------\n");
 	printf("<18.5           Bajo peso\n");
 	printf("18.5 a 24.9     Normal\n");
 	printf("25.0 a 29.9     Sobrepeso\n");
-	printf(">=30            Obesidad\n\n");
+	printf(">=30            Obesidad\n");
 	
-	if(imc<18.5)
-		printf("Su condición es Bajo Peso.");
-	else{
-		if(imc<=24.9)
-			printf("Su condición es Normal.");
-		else{
-			if(imc<=29.9)
-				printf("Su condición es Sobrepeso.");
-			else{
-				printf("Su condición es Obesidad.");
-			}
-		}
-	}
-
 	return 0;
 }
